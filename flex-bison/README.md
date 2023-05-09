c9ea25631d8e3cb2c82d45c7a6ff4224f8efe6a9
Guilherme Rosada <guilhermer4@al.insper.edu.br>
2023-05-08 21:40:00 -0300

    add flex-bison working with success!!

---
+++ flex-bison/README.md
@@ -0,0 +1,13 @@
+# Analisador Sintático com Flex-Bison
+
+Para executar o analisador, basta rodar o programa `analyzer` utilizando o arquivo `teste.py` como entrada ou qualquer outra sequência de códigos.
+
+```
+./analyser < teste.py
+```
+
+Para compilar novamente o executável, basta utilizar o comando abaixo
+
+```
+flex lexer.l && bison -d -t parser.y && gcc lex.yy.c parser.tab.c -o analyzer
+```
---
+++ flex-bison/teste.py
@@ -0,0 +1,51 @@
+declare x = 2
+declare y = 3 - 2 + 1 - 2 / 4 * 5 + x
+
+declare z = x + y
+
+a = X > Y
+b = X < Y
+c = X >= Y
+d = X <= Y
+e = X == Y
+
+se (X == 3) {
+    declare x = 2
+    declare y = 3 - 2 + 1 - 2 / 4 * 5 + x
+
+} ainda se (Y == 3) {
+    imprima("faz algo")
+} senao {
+    imprima("buuu")
+}
+
+se ((a && b) || (c && d)) {
+    imprima("faz algo")
+} senao {
+    imprima("faz nada")
+}
+
+se (((a<3) && (b>3)) || ((c<3) && (d>3))) {
+    imprima("faz algo")
+} senao {
+    imprima("faz nada")
+}
+
+
+enquanto for (x < 10) {
+    imprima("faz algo")
+}
+
+defina funcao (x, y) {
+    imprima("faz algo")
+}
+
+funcao (x, y)
+
+defina funcao_muito_louca123(x, 123, "abc") {
+    imprima("faz algo muito louco")
+}
+
+funcao_muito_louca123(aueieie, 24, "abc")
+
+"tudo azul"
