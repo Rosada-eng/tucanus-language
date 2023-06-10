a8ae68dc6a50aa007cb3709223b9b4b6e30b262d
Guilherme Rosada <62749179+Rosada-eng@users.noreply.github.com>
2023-06-10 10:50:05 -0300

    edit: personalize Token compare method

--- src/Token.py
+++ src/Token.py
@@ -8,3 +8,8 @@ class Token:
 
     def __str__(self):
         return f"Token({self.type}, {self.value})"
+    
+    def __eq__(self, other):
+        if isinstance(other, Token):
+            return self.type == other.type and self.value == other.value
+        return False
