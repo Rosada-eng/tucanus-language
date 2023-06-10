0e828e876563161c7a5ed10f0d1b14dc66633264
Guilherme Rosada <62749179+Rosada-eng@users.noreply.github.com>
2023-06-10 10:36:23 -0300

    init: adaptação compilador de aula para tucanus

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+from src.Parser import Parser
+
+if __name__ == "__main__":
+    try:
+        input_file = sys.argv[1]
+
+    except IndexError:
+        input_file = "./teste.jl"
+
+    finally:
+        with open (input_file, "r") as f:
+            input_str = f.read()
+            
+    Parser.run(input_str)
