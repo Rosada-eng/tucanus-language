612d5b5fb41429d73f2fb86e42ddb39f0ab70559
Guilherme Rosada <62749179+Rosada-eng@users.noreply.github.com>
2023-06-12 13:03:24 -0300

    remove type check and varDec

--- src/Nodes/BinOp.py
+++ src/Nodes/BinOp.py
@@ -17,53 +17,53 @@ class BinOp(Node):
         if child2 is not None:
             self.children.append(child2)
 
-    def evaluate_arithmetic(self, op_type:str, child1: tuple, child2: tuple) -> int:
+    def evaluate_arithmetic(self, op_type:str, child1_value: int, child2_value: int) -> int:
         
         if op_type == "PLUS":
-            return child1[1] + child2[1]
+            return child1_value + child2_value
         elif op_type == "MINUS":
-            return child1[1] - child2[1]
+            return child1_value - child2_value
         elif op_type == "MULTIPLY":
-            return child1[1] * child2[1]
+            return child1_value * child2_value
         elif op_type == "DIVIDER":
-            return child1[1] // child2[1]
+            return child1_value // child2_value
 
-    def evaluate_logic(self, op_type:str, child1: tuple, child2: tuple) -> int:
+    def evaluate_logic(self, op_type:str, child1_value: int, child2_value: int) -> int:
         if op_type == "LESSER_THAN":
-            result = child1[1] < child2[1]
+            result = child1_value < child2_value
         elif op_type == "GREATER_THAN":
-            result = child1[1] > child2[1]
+            result = child1_value > child2_value
         elif op_type == "EQUALS":
-            result = child1[1] == child2[1]
+            result = child1_value == child2_value
         
         elif op_type == "AND":
-            result = child1[1] and child2[1]
+            result = child1_value and child2_value
         elif op_type == "OR":
-            result = child1[1] or child2[1]
+            result = child1_value or child2_value
 
         if result:
             return 1
         return 0
     
-    def evaluate_string_ops(self, op_type:str, child1: tuple, child2: tuple) -> str:
+    def evaluate_string_ops(self, op_type:str, child1_value: str, child2_value: str) -> str:
         if op_type == "CONCAT":
-            return str(child1[1]) + str(child2[1])
+            return str(child1_value) + str(child2_value)
     
     def evaluate(self, symbol_table:SymbolTable) -> tuple:
-        child1 = self.children[0].evaluate(symbol_table)
-        child2 = self.children[1].evaluate(symbol_table)
+        child1_value = self.children[0].evaluate(symbol_table)
+        child2_value = self.children[1].evaluate(symbol_table)
 
         # OPERAÇÕES ARITMÉTICAS
         if self.value in ["PLUS", "MINUS", "MULTIPLY", "DIVIDER"]:
-            return self.evaluate_arithmetic(self.value, child1, child2)
+            return self.evaluate_arithmetic(self.value, child1_value, child2_value)
         
         # OPERAÇÕES LÓGICAS
         elif self.value in ["LESSER_THAN", "GREATER_THAN", "EQUALS", "AND", "OR"]:
-            return self.evaluate_logic(self.value, child1, child2)
+            return self.evaluate_logic(self.value, child1_value, child2_value)
 
         # OPERAÇÕES COM STRING
         elif self.value == "CONCAT":
-            return self.evaluate_string_ops(self.value, child1, child2)
+            return self.evaluate_string_ops(self.value, child1_value, child2_value)
         
         else:
             raise ValueError("Binary Operator has an unexpected operator type: ", self.value)
